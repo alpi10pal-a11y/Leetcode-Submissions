bbbb9609168ad18e6ab30e6ce6c3fe143021677d
Alpi Pal <alpi10.pal@gmail.com>
2026-03-20 15:34:01 +0530

    Time: 2 ms (41.4%), Space: 10.7 MB (40.43%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reverseWords(string s) {
+        stack<string> st;
+        string word = "";
+        for(int i = 0; i < s.length(); i++) {
+            if(s[i] == ' ') {
+                if(word != "") {
+                    st.push(word);
+                    word = "";
+                }
+            } else {
+                word += s[i];
+            }
+        }
+        if(word != "") {
+            st.push(word);
+        }
+        string result = "";
+        while(!st.empty()) {
+            result += st.top();
+            st.pop();
+            if(!st.empty()) result += " ";
+        }
+        return result;
+    }
+};
