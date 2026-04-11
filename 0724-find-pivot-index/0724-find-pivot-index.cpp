b8d608b0e9b63da6ae52cba3b80952f5ce73c06e
Alpi Pal <alpi10.pal@gmail.com>
2026-04-11 20:50:30 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.cpp
+++ 0724-find-pivot-index/0724-find-pivot-index.cpp
@@ -5,13 +5,13 @@ public:
         for(int i =0;i<nums.size();i++){
                  sum+=nums[i];
         }
-        int cs = 0;
+        int ls = 0;
         for(int i =0;i<nums.size();i++){
-            int ls=cs;
-          int right = sum-cs-nums[i];
+            
+          int right = sum-ls-nums[i];
           if(ls==right) 
             return i;
-            cs+=nums[i];
+            ls+=nums[i];
 
         }
         return -1;
